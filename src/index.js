10df82d0786880f1c9940648cded40d936727e65
vadim-mg <vadikm1981@yandex.ru>
2023-01-23 21:19:02 +0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = bracketsConfig.reduce((acc, val, i) => {
+    const [op, cl] = val
+    acc[op] = {
+      isOpen: true,
+      close: cl,
+    }
+    if (op !== cl) {
+      acc[cl] = {
+        isOpen: false,
+        open: op,
+      }
+    } else {
+      acc[op]["open"] = op
+    }
+    return acc
+  }, {})
+
+  const stack = []
+
+  for (let i = 0; i < str.length; i++) {
+    let bracket = brackets[str[i]]
+    if (stack.length && stack[stack.length - 1] === bracket.open) {
+      stack.pop()
+      continue
+    }
+    if (bracket.isOpen) {
+      stack.push(str[i])
+      continue
+    }
+    return false
+  }
+
+  return stack.length ? false : true
 }
